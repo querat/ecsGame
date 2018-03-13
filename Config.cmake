9ffefa2d481cb1c50913f7549f7c7c73aab9f011
queratg <gwendal.querat@gmail.com>
2018-03-13 17:54:15 +0000

    wip

--- Config.cmake
+++
@@ -1,5 +0,0 @@
-# Where to store compiled libs and their headers
-set(EXTERNAL_INSTALL_LOCATION ${CMAKE_BINARY_DIR}/external)
-
-# Compiler warnings
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -Wextra")
