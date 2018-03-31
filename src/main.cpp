a3915e648c50815da75597e260e9c9af06d8a738
queratg <gwendal.querat@gmail.com>
2018-03-31 13:59:21 +0100

    cleaning up main.cpp from unused testing stuff

--- src/main.cpp
+++ src/main.cpp
@@ -3,12 +3,6 @@
 #include "entt/entt.hpp"
 #include "Core.h"
 
-struct myEvent {
-    int data = 42;
-};
-
-struct eventEmitter : public entt::Emitter<eventEmitter> {};
-
 int main()
 {
     Core core;
